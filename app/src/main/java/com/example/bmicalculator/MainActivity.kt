82e44cf3821950492d0e17fa7be5509ef259c61f
ST10438409 <ST10438409@vcconnect.edu.za>
2025-12-16 16:14:10 +0200

    This commit contains the code for the entire project. This commit contains the code that will be used to calculate the actual BMI and display a result.
    Step 1: Its create and declares the ActivityMain binding.
    
    Step 2: Declare and create calculateBMI method.
    
    Step 3: Get the values from the view in the method.
    
    Step 4: Use an IF statement to determine if the value are valid, if not then display an error message to the user.
    
    Step 5: preform the calculation and display the result to the user.

--- app/src/main/java/com/example/bmicalculator/MainActivity.kt
+++ app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -1,12 +1,43 @@
-package com.example.bmicalculator
+//------------------------------------------------------------------------------------------------------------------//
+// Name: Dylan Shortt
+// Group: 1
+// Student Number: ST-10438409
+//------------------------------------------------------------------------------------------------------------------//
 
+//------------------------------------------------------------------------------------------------------------------//
+// <summary>
+// The namespace is used to group classes that are logically related.
+// </summary>
+package com.example.bmicalculator
+//------------------------------------------------------------------------------------------------------------------//
+// <summary>
+// These import statements are used to import the necessary libraries for the program to run.
+// </summary>
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.bmicalculator.databinding.ActivityMainBinding
+import kotlin.math.pow
 
+//------------------------------------------------------------------------------------------------------------------//
+
+//------------------------------------------------------------------------------------------------------------------//
+// <summary>
+// This is the main activity class that is used to create the main activity of the application.
+// </summary>
 class MainActivity : AppCompatActivity() {
+
+    //-------------------------------------------------//
+    // Step 1: set on create binding
+    private lateinit var binding: ActivityMainBinding
+    //-------------------------------------------------//
+
+    //------------------------------------------------------------------------------------------------------------------//
+    // <summary>
+    // This is the onCreate method that is used to create the main activity of the application.
+    // </summary>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +47,62 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        //-------------------------------------------------//
+        // Step 1: set create binding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        //-------------------------------------------------//
+
+        //-------------------------------------------------//
+        // Step 5: set on click listener for button
+        binding.calculateBtn.setOnClickListener {
+            calculateBMI()
+        }
+        //-------------------------------------------------//
     }
+    //------------------------------------------------------------------------------------------------------------------//
+
+    //------------------------------------------------------------------------------------------------------------------//
+    // <summary>
+    // Step 2: create BMI method
+    // </summary>
+    private fun calculateBMI() {
+        //-------------------------------------------------//
+        // Step 3: get values from view
+        val weight = binding.weightEdit.text.toString().toFloatOrNull()
+        val height = binding.heightEdit.text.toString().toFloatOrNull()
+        //-------------------------------------------------//
+
+        //-------------------------------------------------//
+        // Step 4: Determine if values can be calculated or
+        // Skip and display error message
+        if (weight != null && height != null) {
+
+            //-------------------------------------------------//
+            // Preform calculation
+            val bmi = weight / (height / 100).pow(2)
+            val bmiResult = String.format("%.2f", bmi)
+            //-------------------------------------------------//
+
+            //-------------------------------------------------//
+            // create category for BMI
+            val bmiCategory = when {
+                bmi < 18.5 -> "Underweight"
+                bmi < 24.9 -> "Normal weight"
+                bmi < 29.9 -> "Overweight"
+                else -> "Obese"
+            }
+            //-------------------------------------------------//
+
+            binding.resultText.text = "BMI: $bmiResult\nCategory: $bmiCategory"
+        } else {
+            binding.resultText.text = "Invalid input"
+        }
+        //-------------------------------------------------//
+    }
+    //------------------------------------------------------------------------------------------------------------------//
+
 }
+//-----------------------------------...ooo000 END OF FILE 000ooo...-----------------------------------//
